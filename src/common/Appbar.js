9b8e1c1e7a3e8924e0edecd72822ce4953c12a69
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-09-08 21:41:04 +0530

    appbar changes

--- src/common/Appbar.js
+++ src/common/Appbar.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import DotCircle from '../components/DotCircle';
-import CustomButton from './CustomButton';
-import clsx from 'clsx';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { Link } from 'react-scroll';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import DotCircle from "../components/DotCircle";
+import CustomButton from "./CustomButton";
+import clsx from "clsx";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { Link } from "react-scroll";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -23,90 +23,90 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "block",
     },
   },
   tabs: {
     color: theme.palette.pbr.textSecondary,
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "block",
     },
     paddingRight: 10,
     fontWeight: 400,
-    lineHeight: '24px',
-    verticalAlign: 'baseline',
-    letterSpacing: '-1px',
+    lineHeight: "24px",
+    verticalAlign: "baseline",
+    letterSpacing: "-1px",
     margin: 0,
-    padding: '9px 14px 0px',
-    cursor: 'pointer',
-    fontSize: '1.2vw',
+    padding: "9px 14px 0px",
+    cursor: "pointer",
+    fontSize: "1.2vw",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
 
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
   },
   sectionMobile: {
-    width: '100vw',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    width: "100vw",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
   menuIcon: {
-    color: '#212121',
+    color: "#212121",
   },
   list: {
-    width: '250px',
-    borderLeft: '5px solid pink',
+    width: "250px",
+    borderLeft: "5px solid pink",
     borderColor: theme.palette.pbr.primary,
-    height: '100%',
-    backgroundColor: 'transparent',
+    height: "100%",
+    backgroundColor: "transparent",
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
   menuTitle: {
     paddingLeft: 25,
     fontWeight: 500,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
-    textAlign: 'left',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
+    textAlign: "left",
     fontSize: 16,
   },
   menuTitlePink: {
     paddingLeft: 25,
     fontWeight: 500,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
-    textAlign: 'left',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
+    textAlign: "left",
     fontSize: 16,
     color: theme.palette.pbr.primary,
   },
   mobileLink: {
     color: theme.palette.pbr.textSecondary,
-    textDecoration: 'none',
+    textDecoration: "none",
   },
   mobileButton: {
-    borderRadius: '50px',
+    borderRadius: "50px",
     background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    lineHeight: '24px',
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    lineHeight: "24px",
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
-    color: '#ffffff',
-    padding: '5px 15px 5px 15px',
+    color: "#ffffff",
+    padding: "5px 15px 5px 15px",
     fontWeight: 600,
   },
 }));
@@ -125,22 +125,34 @@ export default function PrimarySearchAppBar() {
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
       })}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}>
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
       <List>
         {[
-          { name: 'Solution', id: 'about' },
-          { name: 'How It Works', id: 'how-it-works' },
-          { name: 'PBR Token', id: 'pbt-token' },
+          { name: "Solution", id: "about" },
+          { name: "How It Works", id: "how-it-works" },
+          { name: "PBR Token", id: "pbt-token" },
           // { name: 'Team', id: 'team' },
-          { name: 'Roadmap', id: 'roadmap' },
-          { name: 'Exchange', id: 'exchange' },
+          { name: "Roadmap", id: "roadmap" },
+          { name: "Exchange", id: "exchange" },
         ].map((tab, index) => (
-          <Link activeClass="active" to={tab.id} smooth={true} offset={0} duration={500} delay={0}>
-            <ListItem button key={tab.name} onClick={toggleDrawer(anchor, false)}>
+          <Link
+            activeClass="active"
+            to={tab.id}
+            smooth={true}
+            offset={0}
+            duration={500}
+            delay={0}
+          >
+            <ListItem
+              button
+              key={tab.name}
+              onClick={toggleDrawer(anchor, false)}
+            >
               <ListItemText primary={tab.name} className={classes.menuTitle} />
             </ListItem>
           </Link>
@@ -149,8 +161,11 @@ export default function PrimarySearchAppBar() {
       <Divider />
       <List>
         {[
-          { name: 'Farming', id: 'https://farm.polkabridge.org/' },
-          { name: 'Launchpad', id: 'https://launchpad.polkabridge.org/' },
+          { name: "Farming", id: "https://farm.polkabridge.org/" },
+          {
+            name: "Apply For IDO",
+            id: "https://docs.google.com/forms/d/1ceBZIL8xDNBJWYzZ4j11lhU9MMke8usrNgnPEoSIMf0",
+          },
         ].map((tab, index) => (
           <a href={tab.id} className={classes.mobileLink}>
             <ListItem button key={tab.name}>
@@ -159,8 +174,11 @@ export default function PrimarySearchAppBar() {
           </a>
         ))}
         <ListItem button>
-          <a href="docs/whitepaper.pdf" style={{ textDecoration: 'none' }}>
-            <ListItemText primary={'Read Whitepaper'} className={classes.menuTitlePink} />
+          <a href="docs/whitepaper.pdf" style={{ textDecoration: "none" }}>
+            <ListItemText
+              primary={"Read Whitepaper"}
+              className={classes.menuTitlePink}
+            />
           </a>
         </ListItem>
       </List>
@@ -169,18 +187,35 @@ export default function PrimarySearchAppBar() {
 
   return (
     <div className={classes.grow}>
-      <AppBar position="fixed" style={{ background: 'white', boxShadow: 'none' }}>
+      <AppBar
+        position="fixed"
+        style={{ background: "white", boxShadow: "none" }}
+      >
         <Toolbar className="d-flex justify-content-evenly">
           <Typography className={classes.title} variant="h6" noWrap>
             <img src="logo.png" alt="logo" height="60px" />
           </Typography>
           <div className="d-flex justify-content-end">
-            <Link activeClass="active" to="about" smooth={true} offset={0} duration={500} delay={0}>
+            <Link
+              activeClass="active"
+              to="about"
+              smooth={true}
+              offset={0}
+              duration={500}
+              delay={0}
+            >
               <Typography className={classes.tabs} variant="body1" noWrap>
                 Solution <DotCircle />
               </Typography>
             </Link>
-            <Link activeClass="active" to="how-it-works" smooth={true} offset={0} duration={500} delay={0}>
+            <Link
+              activeClass="active"
+              to="how-it-works"
+              smooth={true}
+              offset={0}
+              duration={500}
+              delay={0}
+            >
               <Typography className={classes.tabs} variant="body1" noWrap>
                 How It Works <DotCircle />
               </Typography>
@@ -190,29 +225,55 @@ export default function PrimarySearchAppBar() {
                 Team <DotCircle />
               </Typography>
             </Link> */}
-            <Link activeClass="active" to="pbt-token" smooth={true} offset={0} duration={500} delay={0}>
+            <Link
+              activeClass="active"
+              to="pbt-token"
+              smooth={true}
+              offset={0}
+              duration={500}
+              delay={0}
+            >
               <Typography className={classes.tabs} variant="body1">
                 PBR Token <DotCircle />
               </Typography>
             </Link>
-            <Link activeClass="active" to="roadmap" smooth={true} offset={0} duration={500} delay={0}>
+            <Link
+              activeClass="active"
+              to="roadmap"
+              smooth={true}
+              offset={0}
+              duration={500}
+              delay={0}
+            >
               <Typography className={classes.tabs} variant="body1">
                 Roadmap <DotCircle />
               </Typography>
             </Link>
-            <Link activeClass="active" to="exchange" smooth={true} offset={0} duration={500} delay={0}>
+            <Link
+              activeClass="active"
+              to="exchange"
+              smooth={true}
+              offset={0}
+              duration={500}
+              delay={0}
+            >
               <Typography className={classes.tabs} variant="body1">
                 Exchange <DotCircle />
               </Typography>
             </Link>
-            <a href="docs/whitepaper.pdf" style={{ textDecoration: 'none' }}>
+            <a href="docs/whitepaper.pdf" style={{ textDecoration: "none" }}>
               <Typography className={classes.tabs} variant="body1">
                 Read Whitepaper <DotCircle />
               </Typography>
             </a>
 
             <div className={classes.sectionDesktop}>
-              <CustomButton title={'LaunchPad'} link={'https://launchpad.polkabridge.org'}></CustomButton>
+              <CustomButton
+                title={"Apply For IDO"}
+                link={
+                  "https://docs.google.com/forms/d/1ceBZIL8xDNBJWYzZ4j11lhU9MMke8usrNgnPEoSIMf0"
+                }
+              ></CustomButton>
             </div>
           </div>
           <div className={classes.sectionMobile}>
@@ -221,13 +282,14 @@ export default function PrimarySearchAppBar() {
             </div>
 
             <div>
-              {['right'].map((anchor) => (
+              {["right"].map((anchor) => (
                 <React.Fragment key={anchor}>
                   <IconButton
                     aria-label="Menu"
                     aria-haspopup="true"
                     className={classes.menuIcon}
-                    onClick={toggleDrawer(anchor, true)}>
+                    onClick={toggleDrawer(anchor, true)}
+                  >
                     <MenuIcon />
                   </IconButton>
 
@@ -236,7 +298,8 @@ export default function PrimarySearchAppBar() {
                     disableSwipeToOpen={false}
                     open={state[anchor]}
                     onClose={toggleDrawer(anchor, false)}
-                    onOpen={toggleDrawer(anchor, true)}>
+                    onOpen={toggleDrawer(anchor, true)}
+                  >
                     {list(anchor)}
                   </SwipeableDrawer>
                 </React.Fragment>
--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -1,16 +1,16 @@
-import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
-import { CallToAction, ShutterSpeed } from '@material-ui/icons';
-import Pulse from 'react-reveal/Pulse';
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { CallToAction, ShutterSpeed } from "@material-ui/icons";
+import Pulse from "react-reveal/Pulse";
 
 const useStyles = makeStyles((theme) => ({
   spacing: {
     paddingTop: 130,
 
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       paddingTop: 50,
-      textAlign: 'center',
+      textAlign: "center",
     },
   },
   circle: {
@@ -18,42 +18,42 @@ const useStyles = makeStyles((theme) => ({
     height: 7,
     padding: 5,
     margin: 6,
-    borderRadius: '50%',
+    borderRadius: "50%",
     backgroundColor: theme.palette.pbr.primary,
   },
   header: {
-    height: '80vh',
-    width: 'auto',
+    height: "80vh",
+    width: "auto",
     background: `linear-gradient(0deg, rgba(255, 255, 255, 0.9), rgba(233, 233, 233, 0.99) ),url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAl2T6kWLUzalYEALVytfVGJPuyw9eQ_wYHrvHv1r5xQrn8qebMgwudwpZNsQyXrtJM_M&usqp=CAU")`,
-    backgroundSize: 'contain',
-    [theme.breakpoints.up('sm')]: {
-      height: '85vh',
+    backgroundSize: "contain",
+    [theme.breakpoints.up("sm")]: {
+      height: "85vh",
     },
   },
   notice: {
     fontWeight: 400,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
     paddingBottom: 15,
   },
   heading: {
     color: theme.palette.pbr.textSecondary,
-    width: 'auto',
-    textAlign: 'center',
-    fontSize: '52px',
+    width: "auto",
+    textAlign: "center",
+    fontSize: "52px",
 
     fontWeight: 600,
-    verticalAlign: 'middle',
-    wordSpacing: '0px',
-    margin: '0px 0px 12px',
-    padding: '0px 12px',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '32px',
+    verticalAlign: "middle",
+    wordSpacing: "0px",
+    margin: "0px 0px 12px",
+    padding: "0px 12px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "32px",
     },
   },
   subheading: {
-    textAlign: 'center',
+    textAlign: "center",
     paddingTop: 20,
     fontSize: 16,
     fontWeight: 400,
@@ -61,26 +61,26 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 25,
   },
   button: {
-    borderRadius: '50px',
+    borderRadius: "50px",
     background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    lineHeight: '24px',
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    lineHeight: "24px",
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
     marginTop: 15,
-    color: '#ffffff',
-    padding: '15px 30px 15px 30px',
+    color: "#ffffff",
+    padding: "15px 30px 15px 30px",
     fontWeight: 600,
     fontSize: 14,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       fontSize: 13,
-      padding: '10px 20px 10px 20px',
+      padding: "10px 20px 10px 20px",
     },
   },
   icon: {
     fontSize: 16,
     marginRight: 7,
-    color: '#ffffff',
+    color: "#ffffff",
   },
 }));
 
@@ -89,7 +89,7 @@ export default function Header() {
   return (
     <Fragment>
       <section>
-        {' '}
+        {" "}
         <div className={classes.header}>
           <div className={classes.spacing}>
             <div className="d-flex flex-row justify-content-center mt-5">
@@ -104,7 +104,7 @@ export default function Header() {
               </h1>
             </Pulse>
             <p className={classes.subheading}>
-              <strong>Powered by </strong>{' '}
+              <strong>Powered by </strong>{" "}
               <div className={classes.title} variant="h6" noWrap>
                 <img
                   src="https://polkadot.network/assets/img/logo-polkadot.svg?v=c1f8875403"
@@ -114,21 +114,28 @@ export default function Header() {
               </div>
             </p>
             <div className="text-center mt-3">
-              <a href="https://farm.polkabridge.org/" style={{ textDecoration: 'none', padding: 5 }}>
-                <Button variant="contained" color="primary" className={classes.button}>
+              <a
+                href="https://farm.polkabridge.org/"
+                style={{ textDecoration: "none", padding: 5 }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                >
                   <ShutterSpeed className={classes.icon} /> Farming
                 </Button>
               </a>
-              <a href="https://stake.polkabridge.org/" style={{ textDecoration: 'none', padding: 5 }}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  <ShutterSpeed className={classes.icon} /> Staking
-                </Button>
-              </a>
               <a
-                href="https://docs.google.com/forms/d/1ceBZIL8xDNBJWYzZ4j11lhU9MMke8usrNgnPEoSIMf0"
-                style={{ textDecoration: 'none', padding: 5 }}>
-                <Button variant="contained" color="primary" className={classes.button}>
-                  <CallToAction className={classes.icon} /> Apply for IDO
+                href="https://stake.polkabridge.org/"
+                style={{ textDecoration: "none", padding: 5 }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                >
+                  <ShutterSpeed className={classes.icon} /> Staking
                 </Button>
               </a>
             </div>
