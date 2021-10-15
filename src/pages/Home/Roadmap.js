302e785cca5eeed4a143102626f719873ac7425c
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-10-15 10:51:03 +0530

    roadmap updates

--- src/pages/Home/Roadmap.js
+++ src/pages/Home/Roadmap.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   AccountBalance,
   ChevronLeft,
@@ -11,15 +11,15 @@ import {
   SettingsEthernet,
   SwapHoriz,
   Timer,
-} from '@material-ui/icons';
-import { Fade } from 'react-reveal';
+} from "@material-ui/icons";
+import { Fade } from "react-reveal";
 
 const useStyles = makeStyles((theme) => ({
   background: {
     padding: 40,
-    height: '100%',
-    [theme.breakpoints.down('sm')]: {
-      height: '100%',
+    height: "100%",
+    [theme.breakpoints.down("sm")]: {
+      height: "100%",
       padding: 0,
       paddingBottom: 50,
       paddingTop: 50,
@@ -27,24 +27,24 @@ const useStyles = makeStyles((theme) => ({
   },
   heading: {
     color: theme.palette.pbr.textSecondary,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 36,
     fontWeight: 600,
-    verticalAlign: 'middle',
-    wordSpacing: '0px',
-    [theme.breakpoints.down('sm')]: {
+    verticalAlign: "middle",
+    wordSpacing: "0px",
+    [theme.breakpoints.down("sm")]: {
       fontSize: 28,
     },
   },
   para: {
     fontWeight: 400,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
     paddingTop: 10,
     paddingBottom: 10,
-    textAlign: 'center',
-    [theme.breakpoints.down('sm')]: {
+    textAlign: "center",
+    [theme.breakpoints.down("sm")]: {
       fontSize: 16,
     },
   },
@@ -54,69 +54,69 @@ const useStyles = makeStyles((theme) => ({
   },
 
   imageContainer: {
-    width: '80%',
-    [theme.breakpoints.down('sm')]: {
+    width: "80%",
+    [theme.breakpoints.down("sm")]: {
       marginTop: 30,
-      width: '100%',
+      width: "100%",
     },
   },
   scroll: {},
   roadmapWrapper: {
-    display: 'flex',
-    width: '100%',
+    display: "flex",
+    width: "100%",
     scrollbarWidth: 0,
-    overflowX: 'scroll',
+    overflowX: "scroll",
   },
   quarterWrapper: {
     width: 250,
   },
   lineMaker: {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
     width: 225,
     marginTop: 15,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       width: 180,
     },
   },
   baseline: {
-    display: 'flex',
-    alignSelf: 'center',
+    display: "flex",
+    alignSelf: "center",
     height: 5,
-    width: '100%',
+    width: "100%",
     backgroundColor: theme.palette.pbr.primary,
-    verticalAlign: 'baseline',
+    verticalAlign: "baseline",
   },
   circle: {
     height: 20,
     width: 20,
     backgroundColor: theme.palette.pbr.primary,
-    borderRadius: '50%',
+    borderRadius: "50%",
   },
   icon: {
     fontSize: 48,
-    color: '#616161',
-    '&:hover': {
+    color: "#616161",
+    "&:hover": {
       color: theme.palette.pbr.primary,
     },
   },
   details: {},
   title: {
     fontWeight: 600,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 16,
     marginTop: 15,
     marginBottom: 15,
   },
   quarterText: {
     fontWeight: 600,
-    verticalAlign: 'baseline',
-    letterSpacing: '-0.8px',
+    verticalAlign: "baseline",
+    letterSpacing: "-0.8px",
     margin: 0,
-    textAlign: 'left',
+    textAlign: "left",
     fontSize: 22,
     marginTop: 10,
     marginBottom: 10,
@@ -130,17 +130,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Roadmap(props) {
   const classes = useStyles();
-  const [x, setX] = useState(-300);
+  const [x, setX] = useState(-500);
 
   const goRight = () => {
-    x === -100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 5) ? setX(0) : setX(x - 100);
+    x === -100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 5)
+      ? setX(0)
+      : setX(x - 100);
   };
   const goLeft = () => {
     x === 100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 5) ? setX(x + 100) : setX(0);
   };
 
   const goRightMob = () => {
-    x === -100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 1) ? setX(0) : setX(x - 100);
+    x === -100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 1)
+      ? setX(0)
+      : setX(x - 100);
   };
   const goLeftMob = () => {
     x === 100 * ([1, 2, 3, 4, 5, 6, 7, 8].length - 1) ? setX(x + 100) : setX(0);
@@ -152,14 +156,19 @@ export default function Roadmap(props) {
         <h6 className={classes.heading}>
           PolkaBridge <strong className={classes.highlight}>RoadMap</strong>
         </h6>
-        <p className={classes.para}>Fundamentally strong and visionary project with strong foundation.</p>
+        <p className={classes.para}>
+          Fundamentally strong and visionary project with strong foundation.
+        </p>
       </div>
       <Fade top>
         <div className="mt-5">
           <div className="row g-0 align-items-center ">
             <div className="col-2 col-md-1">
-              {' '}
-              <button className="c--right" onClick={window.innerWidth > 900 ? goLeft : goLeftMob}>
+              {" "}
+              <button
+                className="c--right"
+                onClick={window.innerWidth > 900 ? goLeft : goLeftMob}
+              >
                 <div className="icon-wrapper">
                   <ChevronLeft className={classes.icon} />
                 </div>
@@ -175,7 +184,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q3, 2020</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>Research</h6>
@@ -192,7 +202,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q4, 2020</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>Team & Tokenomics </h6>
@@ -209,7 +220,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q1, 2021</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>Listing & Farming</h6>
@@ -231,7 +243,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q2, 2021</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>Staking & Launchpad</h6>
@@ -239,7 +252,6 @@ export default function Roadmap(props) {
                       <ul className={classes.list}>
                         <li>Multichain Launchpad </li>
                         <li>Multichain Staking</li>
-
                       </ul>
                     </div>
                   </div>,
@@ -249,13 +261,17 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q3, 2021</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>AMM</h6>
 
                       <ul className={classes.list}>
-                        <li>Multichain AMM (Ethereum, Binance Smart Chain, Polygon) </li>
+                        <li>
+                          Multichain AMM (Ethereum, Binance Smart Chain,
+                          Polygon){" "}
+                        </li>
                         <li>Develop testnet on MoonBeam </li>
                         <li>Expand to Polygon Ecosystem</li>
                         <li>Listing on tier-1 CEXs</li>
@@ -268,14 +284,27 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q4, 2021</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>PolkaDot Integration</h6>
 
                       <ul className={classes.list}>
-                        <li>Migrate PBR to MoonBeam parachain</li>
-                        <li>Integrate PolkaDot into PolkaBridge DEFI application (AMM, Staking, Farming, LaunchPad)</li>
+                        <li>
+                          Listing on tier-1 CEX (Kucoin, Huobi, Okex, FTX)
+                        </li>
+                        <li>Mainnet Multichain AMM</li>
+                        <li>
+                          Integrate Harmony Blockchain into PolkaBridge DEFI
+                          application(AMM, Staking, LaunchPad)
+                        </li>
+                        <li>Develop testnet on MoonBeam</li>
+                        <li>Run PBR to MoonBeam parachain</li>
+                        <li>
+                          Integrate PolkaDot into PolkaBridge DEFI application
+                          (AMM, Staking, LaunchPad)
+                        </li>
                       </ul>
                     </div>
                   </div>,
@@ -285,7 +314,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q1, 2022</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>Crosschain AMM</h6>
@@ -301,7 +331,8 @@ export default function Roadmap(props) {
                       <h4 className={classes.quarterText}>Q2, 2022</h4>
                     </div>
                     <div className={classes.lineMaker}>
-                      <div className={classes.circle}></div> <div className={classes.baseline}></div>
+                      <div className={classes.circle}></div>{" "}
+                      <div className={classes.baseline}></div>
                     </div>
                     <div className={classes.details}>
                       <h6 className={classes.title}>New Features</h6>
@@ -314,7 +345,11 @@ export default function Roadmap(props) {
                   </div>,
                 ].map((item, index) => {
                   return (
-                    <div key={index} className="slide" style={{ transform: `translateX(${x}%)` }}>
+                    <div
+                      key={index}
+                      className="slide"
+                      style={{ transform: `translateX(${x}%)` }}
+                    >
                       {item}
                     </div>
                   );
@@ -323,7 +358,10 @@ export default function Roadmap(props) {
             </div>
 
             <div className="col-2 col-md-1  ">
-              <button className="c--right" onClick={window.innerWidth > 900 ? goRight : goRightMob}>
+              <button
+                className="c--right"
+                onClick={window.innerWidth > 900 ? goRight : goRightMob}
+              >
                 <div className="icon-wrapper">
                   <ChevronRight className={classes.icon} />
                 </div>
