a5c53f949d2408373d105d6798f5f8a44b133485
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-11-16 17:27:01 +0530

    kucoin added

--- src/pages/Home/Listings.js
+++ src/pages/Home/Listings.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   },
   imageWrapper: {
     padding: 15,
-    height: "55px",
+    height: "58px",
   },
   heading: {
     color: theme.palette.pbr.textSecondary,
@@ -37,13 +37,12 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.pbr.primary,
   },
   listingsWrapper: {
-    width: 800,
+    width: 920,
     border: "1px solid #e9e9e9",
     borderRadius: 7,
     boxShadow: `0 2.8px 2.2px rgba(0, 0, 0, 0.024),
-
-  0 2.8px 3.4px rgba(0, 0, 0, 0.086),
-  0 10px 10px rgba(0, 0, 0, 0.02)`,
+     0 2.8px 3.4px rgba(0, 0, 0, 0.086),
+     0 10px 10px rgba(0, 0, 0, 0.02)`,
     [theme.breakpoints.down("sm")]: {
       width: "100%",
     },
@@ -51,6 +50,12 @@ const useStyles = makeStyles((theme) => ({
   cmcLogo: {
     backgroundColor: "white",
     borderRadius: 10,
+    height: 55,
+    padding: 10,
+  },
+  kucoinLogo: {
+    backgroundColor: "white",
+
     height: 55,
     padding: 10,
   },
@@ -66,7 +71,7 @@ export default function Listings() {
       <div className={classes.listingsWrapper}>
         <div className="mt-3 row g-0 text-center">
           <Fade left>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-3 my-2">
               <a href="https://app.uniswap.org/#/swap?inputCurrency=0x298d492e8c1d909d3f63bc4a36c66c64acb3d695">
                 <img
                   src="images/uniswap.png"
@@ -75,12 +80,21 @@ export default function Listings() {
                 />
               </a>
             </div>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-2 my-2">
               <a href="https://quickswap.exchange/#/swap?inputCurrency=0x0d6ae2a429df13e44a07cd2969e085e4833f64a0&outputCurrency=0xc2132d05d31c914a87c6611c10748aeb04b58e8f">
                 <img src="images/quickswap.png" className={classes.cmcLogo} />
               </a>
             </div>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-2 my-2">
+              <a href="https://trade.kucoin.com/trade/PBR-USDT">
+                <img
+                  src="images/kucoin.png"
+                  alt="kucoin"
+                  className={classes.kucoinLogo}
+                />
+              </a>
+            </div>
+            <div className="col-6 col-md-2 my-2">
               <a href="https://www.gate.io/trade/PBR_USDT">
                 <img
                   src="images/gate.png"
@@ -89,7 +103,7 @@ export default function Listings() {
                 />
               </a>
             </div>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-3 my-2">
               <a href="https://www.mexc.com/exchange/PBR_USDT">
                 <img
                   src="images/mxc-logo.png"
@@ -98,29 +112,30 @@ export default function Listings() {
                 />
               </a>
             </div>
-            <div className="col-6 col-md-2">
+
+            <div className="col-6 col-md-3 my-2">
               <a href="https://www.bkex.com/#/trade/PBR_USDT">
                 <img src="images/bkex.png" className={classes.cmcLogo} />
               </a>
             </div>
           </Fade>
           <Fade right>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-2 my-2">
               <a href="https://www.bibox.com/en/exchange/basic/PBR_USDT">
                 <img src="images/bibox.webp" className={classes.cmcLogo} />
               </a>
             </div>
-            <div className="col-6 col-md-3">
+            <div className="col-6 col-md-3 my-2">
               <a href="https://www.bitmart.com/trade/en?symbol=PBR_USDT&layout=pro">
                 <img src="images/bitmart.svg" className={classes.cmcLogo} />
               </a>
             </div>
-            <div className="col-6 col-md-2">
+            <div className="col-6 col-md-2 my-2">
               <a href="https://www.coingecko.com/en/coins/polkabridge">
                 <img src="images/coingecko.webp" className={classes.cmcLogo} />
               </a>
             </div>
-            <div className="col-6 col-md-2">
+            <div className="col-6 col-md-2 my-2">
               <a href="https://coinmarketcap.com/currencies/polkabridge/">
                 <img src="images/cmc.png" className={classes.cmcLogo} />
               </a>
