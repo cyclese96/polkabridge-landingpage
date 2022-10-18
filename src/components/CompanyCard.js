652ad4ec4e6b68a385d5498d41689d84ee82c174
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-10-19 00:22:03 +0530

    Implemented the cool looking hover ui
    The only thing left in this the actual social links of the companies and their description.
    
    The file where the company details for VC and Portfolio is present in /pages/Ventures/companyDetails.js
    
    Please add the social links and if a particular social media site is not used by that company then leave a empty string.
    
    Also add a small description and change the default one.

---
+++ src/components/CompanyCard.js
@@ -0,0 +1,195 @@
+import { makeStyles } from "@material-ui/core";
+import {
+  Facebook,
+  Instagram,
+  Language,
+  Telegram,
+  Twitter,
+} from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    borderRadius: "10px",
+    aspectRatio: "5/3",
+    margin: "10px",
+    marginBottom: "30px",
+    perspective: "1000px",
+    transformStyle: "preserve-3d",
+
+    "&:hover": {
+      boxShadow: "0 .25rem 1rem rgb(186, 45, 125, 0.8)",
+      border: "0.5px solid rgb(186, 45, 125)",
+    },
+    "&:hover $cardBody": {
+      transform: "rotateY(-180deg)",
+    },
+
+    [theme.breakpoints.down("sm")]: {
+      aspectRatio: "16/8",
+      margin: "5px",
+      marginBottom: "10px",
+    },
+  },
+  cardBody: {
+    position: "relative",
+    height: "100%",
+    width: "100%",
+    transition:
+      "transform .7s cubic-bezier(0.4, 0.2, 0.2, 1), -webkit-transform .7s cubic-bezier(0.4, 0.2, 0.2, 1)",
+    transformStyle: "preserve-3d",
+
+    borderRadius: "10px",
+    backgroundImage:
+      "linear-gradient(rgb(23, 29, 42) 0%, rgba(23, 29, 42, 0.4) 100%)",
+  },
+  cardFront: {
+    position: "relative",
+    backfaceVisibility: "hidden",
+    WebkitBackfaceVisibility: "hidden",
+    height: "100%",
+    width: "100%",
+    overflow: "hidden",
+
+    transform: "rotateX(0deg)",
+    padding: "1rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardBack: {
+    backfaceVisibility: "hidden",
+    WebkitBackfaceVisibility: "hidden",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    transform: "rotateX(0deg)",
+
+    overflow: "hidden",
+
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding: "1rem 1.8rem",
+    transform: "rotateY(180deg)",
+    zIndex: 1,
+  },
+  logo: {
+    width: "60%",
+    margin: "0.5rem 2rem",
+    objectFit: "contain",
+    aspectRatio: "1/1",
+    [theme.breakpoints.down("sm")]: {
+      width: "40%",
+      margin: "0.5rem 1rem",
+    },
+  },
+  cardBack_Header: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "0.5rem",
+  },
+  title: {
+    fontSize: "1rem",
+    fontWeight: "bold",
+    color: "white",
+
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.9rem",
+    },
+  },
+  logoSmall: {
+    width: "40px",
+    objectFit: "contain",
+    marginRight: "1rem",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "30px",
+    },
+  },
+  description: {
+    fontSize: "0.9rem",
+    color: "white",
+    marginBottom: "0.5rem",
+
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.8rem",
+    },
+  },
+  socials: {
+    display: "flex",
+  },
+  social: {
+    margin: "0 0.5rem",
+  },
+}));
+
+const CompanyCard = ({ name, description, logo, socials }) => {
+  const classes = useStyles();
+
+  const Icon = (social) => {
+    switch (social) {
+      case "website":
+        return <Language fontSize="small" color="white" />;
+      case "twitter":
+        return <Twitter fontSize="small" color="white" />;
+      case "facebook":
+        return <Facebook fontSize="small" color="white" />;
+      case "instagram":
+        return <Instagram fontSize="small" color="white" />;
+      case "telegram":
+        return <Telegram fontSize="small" color="white" />;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={classes.card}>
+      <div className={classes.cardBody}>
+        <div className={classes.cardFront}>
+          <img src={logo} alt={`${name} Logo`} className={classes.logo} />
+        </div>
+        <div className={classes.cardBack}>
+          <div className={classes.cardBack_Header}>
+            <img
+              src={logo}
+              alt={`${name} Logo`}
+              className={classes.logoSmall}
+            />
+            <div className={classes.title}>{name}</div>
+          </div>
+          <div className={classes.description}>
+            {description.split(" ").length > 8
+              ? description.split(" ").slice(0, 8).join(" ") + "..."
+              : description}
+          </div>
+          <div>
+            {Object.keys(socials).map((key, index) => {
+              if (socials[key].length) {
+                return (
+                  <a
+                    target="_blank"
+                    href={socials[key]}
+                    className={classes.social}
+                  >
+                    {Icon(key)}
+                  </a>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyCard;
---
+++ src/pages/Ventures/companyDetails.js
@@ -0,0 +1,316 @@
+export const ventureDetails = [
+  {
+    name: "Unity Capital Ventures",
+    logo: "https://unitycapitalvc.com/assets/theme-assets/images-3d-animation/uniticapitial.png",
+    description:
+      "Unity Capital Ventures is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://unitycapitalvc.com/",
+      twitter: "sf",
+      facebook: "",
+      instagram: "daf",
+      telegram: "",
+    },
+  },
+  {
+    name: "Oddiyyana Ventures",
+    logo: "./images/oddiyana.jpeg",
+    description:
+      "Oddiyyana Ventures is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://oddiyana.ventures/",
+      twitter: "",
+      facebook: "sdsaf",
+      instagram: "dafj",
+      telegram: "saf",
+    },
+  },
+  {
+    name: "V2B Labs",
+    logo: "https://www.v2blabs.com/_nuxt/img/Logo.6837bc2.png",
+    description:
+      "V2B Labs is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://www.v2blabs.com/",
+      twitter: "dfadf",
+      facebook: "",
+      instagram: "",
+      telegram: "asdf",
+    },
+  },
+  {
+    name: "BMW Capital",
+    logo: "images/bmwcapital.png",
+    description:
+      "BMW Capital is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "http://www.bmw-vc.com/",
+      twitter: "",
+      facebook: "",
+      instagram: "dfajkl",
+      telegram: "df",
+    },
+  },
+  {
+    name: "Zephyrus Capital",
+    logo: "https://zephyruscapital.com/wp-content/uploads/2021/12/logowhite.png",
+    description:
+      "Zephyrus Capital is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://zephyruscapital.com/",
+      twitter: "",
+      facebook: "dsafa",
+      instagram: "dsaf",
+      telegram: "",
+    },
+  },
+  {
+    name: "Unreal Capital",
+    logo: "https://static.tildacdn.com/tild3834-3661-4463-b238-393837653962/UnrealCapital_logo_v.svg",
+    description:
+      "Unreal Capital is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://ucapital.vc/",
+      twitter: "",
+      facebook: "dfajd",
+      instagram: "",
+      telegram: "sfa",
+    },
+  },
+  {
+    name: "Next Capital",
+    logo: "/images/NEXTWHTWIDE.svg",
+    description:
+      "Next Capital is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://www.nextcapital.tech/",
+      twitter: "dafls",
+      facebook: "dfad",
+      instagram: "",
+      telegram: "dsaf",
+    },
+  },
+  {
+    name: "Cogitent Ventures",
+    logo: "/images/cogitent.png",
+    description:
+      "Cogitent Ventures is a venture capital firm that invests in early-stage startups in the blockchain and crypto space.",
+    socials: {
+      website: "https://cogitent.ventures/",
+      twitter: "fjda",
+      facebook: "",
+      instagram: "",
+      telegram: "",
+    },
+  },
+];
+
+export const portfolioDetails = [
+  {
+    name: "Aether Games",
+    logo: "https://www.aethergames.io/static/media/Logo.6d355d0b397d1925df34.png",
+    description: "Aether Games is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://www.aethergames.io/",
+      twitter: "sf",
+      facebook: "",
+      instagram: "daf",
+      telegram: "",
+    },
+  },
+  {
+    name: "Playzap Games",
+    logo: "https://uploads-ssl.webflow.com/622b71d5703c41011dd1c3ac/623294d6f64369568f797a85_footer-logo.png",
+    description: "Playzap Games is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://www.playzap.games/",
+      twitter: "",
+      facebook: "sdsaf",
+      instagram: "dafj",
+      telegram: "saf",
+    },
+  },
+  {
+    name: "Magpie",
+    logo: "https://uploads-ssl.webflow.com/629b39872d72ff02443a529f/629b4145d174c694099716c5_Logo.svg",
+    description:
+      'Magpie is a "decentralized, open-source, and community-driven" NFT marketplace.',
+    socials: {
+      website: "https://www.magpiefi.xyz/",
+      twitter: "dfadf",
+      facebook: "",
+      instagram: "",
+      telegram: "asdf",
+    },
+  },
+  {
+    name: "Web3Port",
+    logo: "images/web3port.png",
+    description: "Web3Port is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://www.web3port.us/",
+      twitter: "",
+      facebook: "",
+      instagram: "dfajkl",
+      telegram: "df",
+    },
+  },
+  {
+    name: "Astra Protocol",
+    logo: "https://astraprotocol.com/wp-content/themes/astra_wp/assets/images/logo.svg",
+    description: "Astra Protocol is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://astraprotocol.com/",
+      twitter: "",
+      facebook: "dsafa",
+      instagram: "dsaf",
+      telegram: "",
+    },
+  },
+  {
+    name: "Tatsumeeko",
+    logo: "images/tatsumeeko.png",
+    description: "Tatsumeeko is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://tatsumeeko.com/",
+      twitter: "",
+      facebook: "dfajd",
+      instagram: "",
+      telegram: "sfa",
+    },
+  },
+  {
+    name: "BitsCrunch",
+    logo: "https://bitscrunch.com/wp-content/themes/bitscrunch/img/logo.svg",
+    description: "BitsCrunch is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://bitscrunch.com/",
+      twitter: "dafls",
+      facebook: "dfad",
+      instagram: "",
+      telegram: "dsaf",
+    },
+  },
+  {
+    name: "Peaq Network",
+    logo: "images/peaq.png",
+    description: "Peaq Network is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://www.peaq.network/",
+      twitter: "fjda",
+      facebook: "",
+      instagram: "",
+      telegram: "",
+    },
+  },
+  {
+    name: "ZKCross",
+    logo: "images/zkcr.png",
+    description: "ZKCross is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://www.zkcross.org/",
+      twitter: "fjda",
+      facebook: "",
+      instagram: "",
+      telegram: "",
+    },
+  },
+  {
+    name: "Humanode",
+    logo: "images/Humanode.png",
+    description: "Humanode is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://humanode.io/",
+      twitter: "fjda",
+      facebook: "",
+      instagram: "",
+      telegram: "",
+    },
+  },
+  {
+    name: "Magic Square",
+    logo: "https://magicsquare.io/wp-content/uploads/2022/09/MSQ-Logo-color.svg",
+    description: "Magic Square is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://magicsquare.io/",
+      twitter: "fjda",
+      facebook: "",
+      instagram: "",
+      telegram: "",
+    },
+  },
+  {
+    name: "Cosmic Guild",
+    logo: "images/Cosmic.png",
+    description: "Cosmic Guild is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://cosmicguild.io/",
+      twitter: "",
+      facebook: "dfa",
+      instagram: "",
+      telegram: "dsaf",
+    },
+  },
+  {
+    name: "BlueMove",
+    logo: "https://bluemove.net/static/media/logo-light.9e42d613ce273b89f8bd87d58adee26a.svg",
+    description: "BlueMove is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://bluemove.net/",
+      twitter: "",
+      facebook: "dfa",
+      instagram: "",
+      telegram: "dsaf",
+    },
+  },
+  {
+    name: "Mover",
+    logo: "images/Mover.png",
+    description: "Mover is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://mov3r.xyz/",
+      twitter: "",
+      facebook: "dfa",
+      instagram: "saf",
+      telegram: "dsaf",
+    },
+  },
+  {
+    name: "Exorde Network",
+    logo: "https://uploads-ssl.webflow.com/620398f412d5829aa28fbb86/622625f35016cd6a5613f833_landscape-logo-color-white.png",
+    description: "Exorde Network is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://exorde.network/",
+      twitter: "",
+      facebook: "dfa",
+      instagram: "saf",
+      telegram: "dsaf",
+    },
+  },
+
+  {
+    name: "Alterverse",
+    logo: "https://alterverse.com/wp-content/uploads/2021/01/AlterVerseLogo_Lg_White_NoStroke_400.png",
+    description: "Alterverse is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://alterverse.com/",
+      twitter: "dasfd",
+      facebook: "dfa",
+      instagram: "saf",
+      telegram: "dsaf",
+    },
+  },
+
+  {
+    name: "Entangle Finance",
+    logo: "images/entangle.png",
+    description: "Entangle Finance is a blockchain-based gaming platform.",
+    socials: {
+      website: "https://entangle.fi/",
+      twitter: "",
+      facebook: "dfa",
+      instagram: "saf",
+      telegram: "dsaf",
+    },
+  },
+];
